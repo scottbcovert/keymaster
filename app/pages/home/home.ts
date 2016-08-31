98077929d2d5734404413cd1212853ae4e3c65c0
Scott Covert <scott.b.covert@gmail.com>
2016-08-31 10:44:35 -0400

    Added beginnings of Salesforce integration to homepage

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,15 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import * as force from './force';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
+
+  doorState: string;
+
   constructor(private navCtrl: NavController) {
-  
+  	this.doorState = 'locked';
   }
 }
